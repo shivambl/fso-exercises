be11219adf18b0c18b48af4e5909caff03dd4f6d
ratherlongname <shivam.bansal@live.com>
2024-04-21 13:17:40 +0530

    Refactor App into components

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,28 +1,49 @@
+const Header = (props) => {
+    return (
+        <h1>{props.course}</h1>
+    )
+}
+
+const Content = (props) => {
+    console.log(props);
+    return (
+        <>
+            <p>
+                {props.cinfo.part1} {props.cinfo.exercises1}
+            </p>
+            <p>
+                {props.cinfo.part2} {props.cinfo.exercises2}
+            </p>
+            <p>
+                {props.cinfo.part3} {props.cinfo.exercises3}
+            </p>
+        </>
+    )
+}
+
+const Total = (props) => {
+    return (
+        <p>Number of exercises {props.total}</p>
+    )
+}
+
 const App = () => {
     const course = 'Half Stack application development'
 
-    const part1 = 'Fundamentals of React'
-    const exercises1 = 10
-    
-    const part2 = 'Using props to pass data'
-    const exercises2 = 7
-
-    const part3 = 'State of a component'
-    const exercises3 = 14
+    const cinfo = {
+        part1: 'Fundamentals of React',
+        exercises1: 10,
+        part2: 'Using props to pass data',
+        exercises2: 7,
+        part3: 'State of a component',
+        exercises3: 14
+    }
 
     return (
         <div>
-            <h1>{course}</h1>
-            <p>
-                {part1} {exercises1}
-            </p>
-            <p>
-                {part2} {exercises2}
-            </p>
-            <p>
-                {part3} {exercises3}
-            </p>
-            <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+            <Header course={course} />
+            <Content cinfo={cinfo} />
+            <Total total={cinfo.exercises1 + cinfo.exercises2 + cinfo.exercises3} />
         </div>
     )
 }
