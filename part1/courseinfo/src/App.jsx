39e100cef14aab09f47fec61cd1a5ae69a8a546e
ratherlongname <shivam.bansal@live.com>
2024-04-22 00:48:38 +0530

    Change state datatype

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -8,9 +8,9 @@ const Content = (props) => {
     console.log(props);
     return (
         <>
-            <Part partname={props.cinfo.part1} exercises={props.cinfo.exercises1} />
-            <Part partname={props.cinfo.part2} exercises={props.cinfo.exercises2} />
-            <Part partname={props.cinfo.part3} exercises={props.cinfo.exercises3} />
+            <Part partname={props.parts[0].name} exercises={props.parts[0].exercises} />
+            <Part partname={props.parts[1].name} exercises={props.parts[1].exercises} />
+            <Part partname={props.parts[2].name} exercises={props.parts[2].exercises} />
         </>
     )
 }
@@ -30,22 +30,34 @@ const Total = (props) => {
 }
 
 const App = () => {
-    const course = 'Half Stack application development'
-
-    const cinfo = {
-        part1: 'Fundamentals of React',
-        exercises1: 10,
-        part2: 'Using props to pass data',
-        exercises2: 7,
-        part3: 'State of a component',
-        exercises3: 14
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7
+            },
+            {
+                name: 'State of a component',
+                exercises: 14
+            }
+        ]
     }
 
     return (
         <div>
-            <Header course={course} />
-            <Content cinfo={cinfo} />
-            <Total total={cinfo.exercises1 + cinfo.exercises2 + cinfo.exercises3} />
+            <Header course={course.name} />
+            <Content parts={course.parts} />
+            <Total
+                total={course.parts.reduce(
+                    (ex_sum, part) => ex_sum + part.exercises,
+                    0
+                )}
+            />
         </div>
     )
 }
