d92bb2812d5d2dd247071f2d461cf8f92ee982c2
ratherlongname <shivam.bansal@live.com>
2024-04-26 03:25:18 +0530

    Add features to left right counter

--- part1/followalong/src/App.jsx
+++ part1/followalong/src/App.jsx
@@ -1,32 +1,46 @@
 import { useState } from 'react'
 
-const Display = ({count}) => <p>{count}</p>
+const Display = ({text}) => <p>{text}</p>
 
 const Button = ({ text, onClick }) => <button onClick={onClick}>{text}</button>
 
 const App = () => {
     const [lcount, setLcount] = useState(0)
     const [rcount, setRcount] = useState(0)
+    const [total, setTotal] = useState(0)
+    const [allClicks, setAllClicks] = useState([])
 
-    const handleLeftClick = () => setLcount(lcount + 1)
-    const handleRightClick = () => setRcount(rcount + 1)
+    const handleLeftClick = () => {
+        setAllClicks(allClicks.concat('L'))
+        setLcount(lcount + 1)
+        setTotal(total + 1)
+    }
+    const handleRightClick = () => {
+        setAllClicks(allClicks.concat('R'))
+        setRcount(rcount + 1)
+        setTotal(total + 1)
+    }
     const handleReset = () => {
+        setAllClicks([])
         setLcount(0)
         setRcount(0)
+        setTotal(0)
     }
 
     return (
         <>
-            <Display count={lcount} />
+            <Display text={lcount} />
             <Button
                 text='Left button'
                 onClick={handleLeftClick}
             />
-            <Display count={rcount} />
+            <Display text={rcount} />
             <Button
                 text='Right button'
                 onClick={handleRightClick}
             />
+            <Display text={"Total = " + total} />
+            <Display text={"Click history = " + allClicks.join(', ')} />
             <Button
                 text='Reset All'
                 onClick={handleReset}
