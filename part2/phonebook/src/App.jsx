4e89b15b3610da582f375acfdf9887ebbea822ad
ratherlongname <shivam.bansal@live.com>
2024-04-28 16:22:02 +0530

    Add phone number support

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -5,15 +5,22 @@ const App = () => {
     // State
 
     const [persons, setPersons] = useState([
-        { name: "Ghanshyam Sundaram" }
+        {
+            name: "Ghanshyam Sundaram",
+            number: "9090909090"
+        }
     ])
     const [newName, setNewName] = useState('')
-
+    const [newNumber, setNewNumber] = useState('')
 
     // Handlers
 
     const handleNameChange = (event) => {
         setNewName(event.target.value)
+    }
+
+    const handleNumberChange = (event) => {
+        setNewNumber(event.target.value)
     }
 
     const addPerson = (event) => {
@@ -24,13 +31,17 @@ const App = () => {
 
         if (isPersonAlreadyPresent === undefined) {
             const personObject = {
-                name: newName
+                name: newName,
+                number: newNumber
             }
             setPersons(persons.concat(personObject))
-        } else {
+        }
+        else {
             window.alert(`${newName} is already added to the phonebook`)
         }
+
         setNewName('')
+        setNewNumber('')
     }
 
     return (
@@ -41,12 +52,15 @@ const App = () => {
                     name: <input value={newName} onChange={handleNameChange} />
                 </div>
                 <div>
+                    number: <input value={newNumber} onChange={handleNumberChange} />
+                </div>
+                <div>
                     <button type='submit'>add</button>
                 </div>
             </form>
 
             <h2>Numbers</h2>
-            {persons.map(person => <p key={person.name}>{person.name}</p>)}
+            {persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
         </div>
     )
 }
