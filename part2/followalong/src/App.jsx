812053684e868e662aa475defc3a122afa64d520
ratherlongname <shivam.bansal@live.com>
2024-04-28 15:17:54 +0530

    Add notes using form

--- part2/followalong/src/App.jsx
+++ part2/followalong/src/App.jsx
@@ -1,7 +1,33 @@
+import { useState } from 'react'
 
 import Note from './components/Note'
 
-const App = ({ notes }) => {
+const App = (props) => {
+
+    // State
+    const [notes, setNotes] = useState(props.notes)
+    const [newNote, setNewNote] = useState("")
+
+    // Handlers
+    const addNote = (event) => {
+        event.preventDefault()
+        console.log("Save clicked: ", event.target)
+
+        const noteObject = {
+            id: notes.length + 1,
+            content: newNote,
+            important: Math.random() < 0.5
+        }
+
+        setNotes(notes.concat(noteObject))
+        setNewNote("")
+    }
+
+    const handleNoteChange = (event) => {
+        console.log("Note modified: ", event.target.value)
+        setNewNote(event.target.value)
+    }
+
     return (
         <div>
             <h1>Notes</h1>
@@ -10,6 +36,13 @@ const App = ({ notes }) => {
                     <Note key={note.id} text={note.content} />
                 )}
             </ul>
+            <form onSubmit={addNote}>
+                <input
+                    value={newNote}
+                    onChange={handleNoteChange}
+                />
+                <button type="submit">save</button>
+            </form>
         </div>
     )
 }
